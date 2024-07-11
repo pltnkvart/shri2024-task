72880cde0973aede198c713f33a9e7b184b34c64
pltnkvart <artosi@yandex.ru>
2024-07-11 17:42:08 +0300

    footer memo

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,32 @@
+import { memo } from "react";
+
+export const Footer = memo(() => {
+	return (
+		<footer className="footer">
+			<ul className="footer__list">
+				<li className="footer__item">
+					<a className="footer__link" href="/">
+						Помощь
+					</a>
+				</li>
+				<li className="footer__item">
+					<a className="footer__link" href="/">
+						Обратная связь
+					</a>
+				</li>
+				<li className="footer__item">
+					<a className="footer__link" href="/">
+						Разработчикам
+					</a>
+				</li>
+				<li className="footer__item">
+					<a className="footer__link" href="/">
+						Условия использования
+					</a>
+				</li>
+			</ul>
+
+			<div className="footer__copyright">© 1997–2023 ООО «Яндекс»</div>
+		</footer>
+	);
+});
--- src/index.jsx
+++ src/index.jsx
@@ -4,11 +4,13 @@ import "./styles.css";
 
 import Header from "./components/Header/Header.jsx";
 import Main from "./components/Main/Main.jsx";
+import { Footer } from "./components/Footer/Footer.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("app"));
 root.render(
 	<>
 		<Header />
 		<Main />
+		<Footer />
 	</>
 );
